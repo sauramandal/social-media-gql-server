cea6849782ec3e900a8ef0973493a5dfca4c4cb5
Saura Mandal <saura.mandal1@gmail.com>
2022-01-15 21:30:16 +0530

    Replace functions with arrow functions

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,86 +1,92 @@
-const { AuthenticationError, UserInputError } = require("apollo-server");
-const Post = require("../../models/Post");
-const checkAuth = require("../../utils/check-auth");
+const { AuthenticationError, UserInputError } = require('apollo-server');
+const Post = require('../../models/Post');
+const checkAuth = require('../../utils/check-auth');
 
 module.exports = {
   Query: {
-    getPosts: async () => {
+    async getPosts() {
       try {
-        const posts = await Post.find().sort({ createdAt: -1 }); // Get latest posts
+        const posts = await Post.find().sort({ createdAt: -1 });
         return posts;
       } catch (err) {
         throw new Error(err);
       }
     },
-    getPost: async (_, { postId }) => {
+    async getPost(_, { postId }) {
       try {
         const post = await Post.findById(postId);
         if (post) {
           return post;
         } else {
-          throw new Error("Post not found!");
+          throw new Error('Post not found');
         }
       } catch (err) {
         throw new Error(err);
       }
-    },
+    }
   },
   Mutation: {
-    createPost: async (_, { body }, context) => {
+    async createPost(_, { body }, context) {
       const user = checkAuth(context);
-      if (body.trim() === "") throw new Error("Post body cannot be empty");
-      const { id, username } = user;
+
+      if (body.trim() === '') {
+        throw new Error('Post body must not be empty');
+      }
+
       const newPost = new Post({
         body,
-        user: id,
-        username,
-        createdAt: new Date().toISOString(),
+        user: user.id,
+        username: user.username,
+        createdAt: new Date().toISOString()
       });
 
       const post = await newPost.save();
-      context.pubSub.publish("NEW_POST", {
-        newPost: post,
+
+      context.pubSub.publish('NEW_POST', {
+        newPost: post
       });
+
       return post;
     },
-    deletePost: async (_, { postId }, context) => {
+    async deletePost(_, { postId }, context) {
       const user = checkAuth(context);
+
       try {
         const post = await Post.findById(postId);
         if (user.username === post.username) {
           await post.delete();
-          return "Post deleted successfully";
+          return 'Post deleted successfully';
         } else {
-          throw new AuthenticationError("Action not allowed");
+          throw new AuthenticationError('Action not allowed');
         }
       } catch (err) {
         throw new Error(err);
       }
     },
-    likePost: async (_, { postId }, context) => {
+    async likePost(_, { postId }, context) {
       const { username } = checkAuth(context);
+
       const post = await Post.findById(postId);
       if (post) {
         if (post.likes.find((like) => like.username === username)) {
-          // Post already liked, unlike it
+          // Post already likes, unlike it
           post.likes = post.likes.filter((like) => like.username !== username);
         } else {
-          // Like a post
+          // Not liked, like post
           post.likes.push({
             username,
-            createdAt: new Date().toISOString(),
+            createdAt: new Date().toISOString()
           });
         }
+
         await post.save();
         return post;
-      } else throw new UserInputError("Post not found!");
-    },
+      } else throw new UserInputError('Post not found');
+    }
   },
-  // Resolvers for Subscription fields differ from resolvers for fields of other types.
-  // Specifically, Subscription field resolvers are objects that define a subscribe function
   Subscription: {
     newPost: {
-      subscribe: (_, __, { pubSub }) => pubSub.asyncIterator("NEW_POST"),
-    },
-  },
+      subscribe: (_, __, { pubsub }) => pubsub.asyncIterator('NEW_POST')
+    }
+  }
 };
